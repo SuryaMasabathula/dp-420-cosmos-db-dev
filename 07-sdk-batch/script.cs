0982f1b4d45a12b2e322e3c8db7d0dee7041440b
surya.pendimi <surya.pendimi@gmail.com>
2022-07-29 17:43:30 +0530

    Lab7 Getting a statuscode with Batch Operations

--- 07-sdk-batch/script.cs
+++ 07-sdk-batch/script.cs
@@ -8,13 +8,23 @@ CosmosClient client = new CosmosClient(endpoint, key);
     
 Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
 Container container = await database.CreateContainerIfNotExistsAsync("products", "/categoryId", 400);
-Product saddle = new("0120", "Worn Saddle", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
-Product handlebar = new("012A", "Rusty Handlebar", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
+// Product saddle = new("0120", "Worn Saddle", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
+// Product handlebar = new("012A", "Rusty Handlebar", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
+
+// PartitionKey partitionKey = new ("9603ca6c-9e28-4a02-9194-51cdb7fea816");
+// TransactionalBatch batch = container.CreateTransactionalBatch(partitionKey)
+//     .CreateItem<Product>(saddle)
+//     .CreateItem<Product>(handlebar);
+
+
+Product light = new("012B", "Flickering Strobe Light", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
+Product helmet = new("012C", "New Helmet", "0feee2e4-687a-4d69-b64e-be36afc33e74");
 
 PartitionKey partitionKey = new ("9603ca6c-9e28-4a02-9194-51cdb7fea816");
+
 TransactionalBatch batch = container.CreateTransactionalBatch(partitionKey)
-    .CreateItem<Product>(saddle)
-    .CreateItem<Product>(handlebar);
+    .CreateItem<Product>(light)
+    .CreateItem<Product>(helmet);
 
     using TransactionalBatchResponse response = await batch.ExecuteAsync();
 
