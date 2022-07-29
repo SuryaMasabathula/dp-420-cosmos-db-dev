bb37a757475e883a883e68754fc12502ad609c48
surya.pendimi <surya.pendimi@gmail.com>
2022-07-29 17:12:24 +0530

    All items in the Transaction Batch must have a unique partionKey. else will error out will show in the next commit

--- 07-sdk-batch/script.cs
+++ 07-sdk-batch/script.cs
@@ -1,10 +1,21 @@
 using System;
 using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
-string key = "<cosmos-key>";
+string endpoint = "https://cosmosacctlab3.documents.azure.com:443/";
+string key = "ZrKn4NoAiuAk8ePhNac123qMjyBFiMXPXOcKNjOdNEcbAnAd3TPOKwFRrD63Tyfdqy6cqfJ7DdISSMpTgeBMAQ==";
 
 CosmosClient client = new CosmosClient(endpoint, key);
     
 Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
 Container container = await database.CreateContainerIfNotExistsAsync("products", "/categoryId", 400);
+Product saddle = new("0120", "Worn Saddle", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
+Product handlebar = new("012A", "Rusty Handlebar", "9603ca6c-9e28-4a02-9194-51cdb7fea816");
+
+PartitionKey partitionKey = new ("9603ca6c-9e28-4a02-9194-51cdb7fea816");
+TransactionalBatch batch = container.CreateTransactionalBatch(partitionKey)
+    .CreateItem<Product>(saddle)
+    .CreateItem<Product>(handlebar);
+
+    using TransactionalBatchResponse response = await batch.ExecuteAsync();
+
+    Console.WriteLine($"Status:\t{response.StatusCode}");
