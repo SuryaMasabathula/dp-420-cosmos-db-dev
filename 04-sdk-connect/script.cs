9893f016625df844bbe6fbabd85b22f9b1277a64
surya.pendimi <surya.pendimi@gmail.com>
2022-07-29 16:17:03 +0530

    Cosmos DB CRUD Ops created.

--- 04-sdk-connect/script.cs
+++ 04-sdk-connect/script.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Linq;
+using Microsoft.Azure.Cosmos;
+
+
+string endpoint = "https://cosmosacctlab3.documents.azure.com:443/";
+string key = "ZrKn4NoAiuAk8ePhNac123qMjyBFiMXPXOcKNjOdNEcbAnAd3TPOKwFRrD63Tyfdqy6cqfJ7DdISSMpTgeBMAQ==";
+
+
+CosmosClient client= new (endpoint,key);
+
+AccountProperties account= await client.ReadAccountAsync();
+Console.WriteLine($"Account Name:\t{account.Id}");
+Console.WriteLine($"Primary Region:\t{account.WritableRegions.FirstOrDefault()?.Name}");
--- 06-sdk-crud/script.cs
+++ 06-sdk-crud/script.cs
@@ -1,7 +1,57 @@
 using System;
 using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
-string key = "<cosmos-key>";
+string endpoint = "https://cosmosacctlab3.documents.azure.com:443/";
+string key = "ZrKn4NoAiuAk8ePhNac123qMjyBFiMXPXOcKNjOdNEcbAnAd3TPOKwFRrD63Tyfdqy6cqfJ7DdISSMpTgeBMAQ==";
 
 CosmosClient client = new CosmosClient(endpoint, key);
+Database database= await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+Container container= await database.CreateContainerIfNotExistsAsync("products","/categoryId", 400);
+Console.WriteLine("..............Creating Data using Id and categoryId.....................");
+// Adding a New Cosmos Item
+Product saddle=new() {
+    id = "706cd7c6-db8b-41f9-aea2-0e0c7e8eb009",
+    categoryId = "9603ca6c-9e28-4a02-9194-51cdb7fea816",
+    name = "Road Saddle",
+    price = 45.99d,
+    tags = new string[]
+    {
+        "tan",
+        "new",
+        "crisp"
+    }
+};
+
+await container.CreateItemAsync<Product>(saddle);
+
+Console.WriteLine("..............Reading Data using Id and categoryId.....................");
+string id = "706cd7c6-db8b-41f9-aea2-0e0c7e8eb009";
+
+string categoryId = "9603ca6c-9e28-4a02-9194-51cdb7fea816";
+PartitionKey partitionKey = new (categoryId);
+
+saddle = await container.ReadItemAsync<Product>(id, partitionKey);
+
+Console.WriteLine($"[{saddle.id}]\t{saddle.name} ({saddle.price:C})");
+
+Console.WriteLine("..............Reading Data using Id and categoryId Completed.....................");
+
+Console.WriteLine("..............Update Data .....................");
+
+saddle.price=1000.0d;
+saddle.name="this is the update saddle name";
+await container.UpsertItemAsync<Product>(saddle);
+
+Console.WriteLine("..............Update Data completed....................");
+
+Console.WriteLine("..............Delete Data using Id and categoryId.completed....................");
+
+await container.DeleteItemAsync<Product>(id,partitionKey);
+
+saddle=await container.ReadItemAsync<Product>(id,partitionKey);
+if(saddle is null)
+{
+Console.WriteLine("..............Delete Data using Id and categoryId.completed....................");
+}
+else
+Console.WriteLine("..............Delete Data using Id and categoryId.Failed....................");
