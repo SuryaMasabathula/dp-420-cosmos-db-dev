22f15ef93a62a84cec007348f427fe657a97d80a
surya.pendimi <surya.pendimi@gmail.com>
2022-08-01 20:26:34 +0530

    Lab11,Lab10,Lab 12 Indexing policy

--- 10-paginate-results-sdk/script.cs
+++ 10-paginate-results-sdk/script.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
+string endpoint = "https://cosmosacctlab3.documents.azure.com:443/";
 
-string key = "<cosmos-key>";
+string key = "ZrKn4NoAiuAk8ePhNac123qMjyBFiMXPXOcKNjOdNEcbAnAd3TPOKwFRrD63Tyfdqy6cqfJ7DdISSMpTgeBMAQ==";
 
 CosmosClient client = new CosmosClient(endpoint, key);
 
 Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
 
 Container container = await database.CreateContainerIfNotExistsAsync("products", "/categoryId");
+
+string sql = "SELECT p.id, p.name, p.price FROM products p ";
+
+QueryDefinition query = new (sql);
+
+QueryRequestOptions options = new ();
+
+options.MaxItemCount = 50;
+
+FeedIterator<Product> iterator = container.GetItemQueryIterator<Product>(query, requestOptions: options);
+
+while (iterator.HasMoreResults)
+{
+    FeedResponse<Product> products = await iterator.ReadNextAsync();
+    foreach (Product product in products)
+    {
+        Console.WriteLine($"[{product.id}]\t[{product.name,40}]\t[{product.price,10}]");
+    }
+
+    Console.WriteLine("Press any key for next page of results");
+    Console.ReadKey();  
+}
--- 12-custom-index-policy/script.cs
+++ 12-custom-index-policy/script.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using Microsoft.Azure.Cosmos;
 
-string endpoint = "<cosmos-endpoint>";
+string endpoint = "https://cosmosacctlab3.documents.azure.com:443/";
 
-string key = "<cosmos-key>";
+string key = "ZrKn4NoAiuAk8ePhNac123qMjyBFiMXPXOcKNjOdNEcbAnAd3TPOKwFRrD63Tyfdqy6cqfJ7DdISSMpTgeBMAQ==";
 
 CosmosClient client = new CosmosClient(endpoint, key);
 
-Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
+Database database = await client.CreateDatabaseIfNotExistsAsync("newcosmicworks");
+
+
+IndexingPolicy policy = new ();
+policy.IndexingMode = IndexingMode.Consistent;
+policy.ExcludedPaths.Add(
+    new ExcludedPath{ Path = "/*" }
+);
+policy.IncludedPaths.Add(
+    new IncludedPath{ Path = "/name/?" }
+);
+
+ContainerProperties options = new ("products", "/categoryId");
+options.IndexingPolicy = policy;
+
+Container container = await database.CreateContainerIfNotExistsAsync(options);
+Console.WriteLine($"Container Created [{container.Id}]");
